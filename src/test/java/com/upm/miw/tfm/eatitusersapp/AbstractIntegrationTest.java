69023619b52ece4043457da12eec28e9dcf135b9
Alejandro Cabezas Garríguez <alexcabezas0705@gmail.com>
2022-05-28 15:17:14 +0200

    Add localhost mongodb

---
+++ src/test/java/com/upm/miw/tfm/eatitusersapp/AbstractIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.upm.miw.tfm.eatitusersapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class AbstractIntegrationTest {
+}
--- src/test/java/com/upm/miw/tfm/eatitusersapp/EatItUsersManagementAppApplicationTests.java
+++ src/test/java/com/upm/miw/tfm/eatitusersapp/EatItUsersManagementAppApplicationTests.java
@@ -3,8 +3,7 @@ package com.upm.miw.tfm.eatitusersapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EatItUsersManagementAppApplicationTests {
+class EatItUsersManagementAppApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
