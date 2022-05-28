332510c658b1a479395bc4a61d233e0748a1ab12
AlexCabezas2018 <alexcabezas0705@gmail.com>
2022-05-28 10:51:50 +0200

    Initial commit

---
+++ src/main/java/com/upm/miw/tfm/eatitusersapp/EatItUsersManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.upm.miw.tfm.eatitusersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatItUsersManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatItUsersManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upm/miw/tfm/eatitusersapp/EatItUsersManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upm.miw.tfm.eatitusersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatItUsersManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
